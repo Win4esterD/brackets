f02ba6e51a0c386b75636a1ddfcebfbd5f6dff49
Pavel Didenko <patriot2006_06@mail.ru>
2023-08-03 08:47:09 +0300

    feat: 16 tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const counter = {
+    "(": 0,
+    ")": 0,
+    "{": 0,
+    "}": 0,
+    "[": 0,
+    "]": 0,
+  };
+
+  for (let letter of str) {
+    counter[letter] += 1;
+    if (
+      counter[")"] > counter["("] ||
+      counter["}"] > counter["{"] ||
+      counter["]"] > counter["["]
+    ) {
+      return false;
+    }
+  }
+
+  if (
+    counter["("] !== counter[")"] ||
+    counter["{"] !== counter["}"] ||
+    counter["["] !== counter["]"]
+  ) {
+    return false;
+  }
+
+  // if(counter['('] === 0 && counter['{'] === 0 && counter['['] === 0 && counter[')'] === 0 && counter['}'] === 0 && counter[']'] === 0){
+  //   return false;
+  // }
+
+  return true;
+};
